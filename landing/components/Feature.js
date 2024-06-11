0937aca43e9194ca6c97444e2adbc5c9276f568c
SnowsFE <snoerkr@gmail.com>
2024-06-11 17:45:04 +0900

    기능 요소 추가

--- landing/components/Feature.js
+++ landing/components/Feature.js
@@ -5,11 +5,12 @@ import getScrollAnimation from "../utils/getScrollAnimation";
 import ScrollAnimationWrapper from "./Layout/ScrollAnimationWrapper";
 
 const features = [
-  "Powerfull online protection.",
-  "Internet without borders.",
-  "Supercharged VPN",
-  "No specific time limits."
-]
+  "자동 매매 봇",
+  "개인화된 투자 전략 제공",
+  "친환경적인 인터페이스",
+  "손쉬운 가상자산 매매",
+  "AI 기반 암호화폐 트레이딩 서비스 제공",
+];
 
 const Feature = () => {
   const scrollAnimation = useMemo(() => getScrollAnimation(), []);
@@ -23,9 +24,7 @@ const Feature = () => {
         <ScrollAnimationWrapper className="flex w-full justify-end">
           <motion.div className="h-full w-full p-4" variants={scrollAnimation}>
             <Image
-              src="/assets/Illustration2.png"
-              alt="VPN Illustrasi"
-              layout="responsive"
+              src="/assets/buysell.png"
               quality={100}
               height={414}
               width={508}
@@ -33,34 +32,36 @@ const Feature = () => {
           </motion.div>
         </ScrollAnimationWrapper>
         <ScrollAnimationWrapper>
-
-        <motion.div className="flex flex-col items-end justify-center ml-auto w-full lg:w-9/12" variants={scrollAnimation}>
-          <h3 className="text-3xl lg:text-4xl font-medium leading-relaxed text-black-600">
-            We Provide Many Features You Can Use
-          </h3>
-          <p className="my-2 text-black-500">
-            You can explore the features that we provide with fun and have their
-            own functions each feature.
-          </p>
-          <ul className="text-black-500 self-start list-inside ml-8">
-            {features.map((feature, index) => (
-              <motion.li
-                className="relative circle-check custom-list"
-                custom={{duration: 2 + index}}
-                variants={scrollAnimation}
-                key={feature}
-                whileHover={{
-                scale : 1.1,
-                transition: {
-                  duration: .2
-                }
-                }}>
+          <motion.div
+            className="flex flex-col items-end justify-center ml-auto w-full lg:w-9/12"
+            variants={scrollAnimation}
+          >
+            <h3 className="text-3xl lg:text-4xl font-medium leading-relaxed text-black-600">
+              기존과 다른 차별화된 기능!
+            </h3>
+            <p className="my-2 text-black-500">
+              자동화된 시스템의 새로운 기능을 경험하고 싶으시다면 당장
+              코인브로를 시작하세요!
+            </p>
+            <ul className="text-black-500 self-start list-inside ml-8">
+              {features.map((feature, index) => (
+                <motion.li
+                  className="relative circle-check custom-list"
+                  custom={{ duration: 2 + index }}
+                  variants={scrollAnimation}
+                  key={feature}
+                  whileHover={{
+                    scale: 1.1,
+                    transition: {
+                      duration: 0.2,
+                    },
+                  }}
+                >
                   {feature}
-              </motion.li>
-              )
-            )}
-          </ul>
-        </motion.div>
+                </motion.li>
+              ))}
+            </ul>
+          </motion.div>
         </ScrollAnimationWrapper>
       </div>
     </div>
